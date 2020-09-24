7573561f4d9deddef9eb121b5d8fafd4bd6c41cb
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-24 09:11:08 +0900

    Bsic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_premitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
 
   def configure_premitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :phone_num, :birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
 
