13dcb9da8492f6b15a5e4be6dd54cfb9c2a5ca74
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-29 17:17:15 +0900

    active hashの追加

---
+++ app/models/shipday.rb
@@ -0,0 +1,7 @@
+class Shipday < ActiveHash::Base
+  self.data = [
+    { id: 1, name: "選択してください"}, { id: 2, :name: "１〜２日で発送"},
+    { id: 3, :name: "２〜３日で発送"}, { id: 4, :name: "４〜７日で発送"}, 
+    { id: 5, :name: "7日以上"},
+  ]
+end
