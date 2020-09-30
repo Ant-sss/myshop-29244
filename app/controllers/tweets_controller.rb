acc1efab76bdaa2a17d94ff9bc0118f981a92f28
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-30 16:00:22 +0900

    tweet削除機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,9 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :destroy]
 
   def index
     @shop = Shop.find(params[:shop_id])
-    @tweets = @shop.tweets
+    @tweets = @shop.tweets.order("created_at DESC")
   end
 
   def new
@@ -18,10 +19,25 @@ class TweetsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def destroy
+    if @tweet.destroy
+      redirect_to shop_tweets_path(@tweet.shop.id)
+    else
+      render :show
+    end
+  end
+
   private
 
   def tweet_params
     params.permit(:title, :text, :image).merge(shopkeeper_id: current_shopkeeper.id, shop_id: params[:shop_id])
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
