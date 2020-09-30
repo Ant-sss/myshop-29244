857ffb35c0a5845c0b5d09edc1ce30f63e7d0b82
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-30 10:15:27 +0900

    tweet投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,4 +5,23 @@ class TweetsController < ApplicationController
     @tweets = @shop.tweets
   end
 
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to shop_tweets_path(@tweet.shop.id)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.permit(:title, :text, :image).merge(shopkeeper_id: current_shopkeeper.id, shop_id: params[:shop_id])
+  end
+
 end
