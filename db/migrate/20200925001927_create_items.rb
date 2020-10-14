fd55039a7baaf39ae23205976a950af3a4168ea2
Ant-sss <mimo.antownette.0429@gmail.com>
2020-10-14 11:51:14 +0900

    stockカラム追加

--- db/migrate/20200925001927_create_items.rb
+++ db/migrate/20200925001927_create_items.rb
@@ -8,7 +8,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :size,               null: false
       t.integer :shipday_id,      null: false
       t.integer :shipfrom_id,     null: false
-      t.text :imformation,         null: false
+      t.text :imformation,        null: false
+      t.integer :stock,           null: false
       t.references :shopkeeper,   null: false, foreign_key: true
       t.references :shop,         null: false, foreign_key: true
       t.timestamps
